24c36067f2d77efa83366fbf48e5be30fac822ba
Anatoliy Ostrovskiy <forgetthisbox@gmail.com>
2014-09-26 22:04:12 +0400

    calling `true` module with try/catch
    To prevent unknown issues with cli it's good to handle errors inside business logic of the app

--- bin/cli.js
+++ bin/cli.js
@@ -2,9 +2,14 @@
 
 var t = require('../index')
 
-if (t()) {
-  process.exit(0);
-}
-else {
+try {
+  if (t()) {
+      process.exit(0);
+  } else {
+      throw new Error('True is false');
+  }
+} catch (err) {
+  console.log(err.message);
+  console.log(err.stack);
   process.exit(1);
 }
