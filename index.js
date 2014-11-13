32c1db2e311a25f298b8fc74986c18ed5297fc4d
Andrii Shumada <eagleeyes91@gmail.com>
2014-11-13 14:21:53 +0200

    performance improvement, add .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Cache as a local var for important perf improvements
-var t = true;
+var t;
 module.exports = function aJavaScriptPortOfTheUnixUtilityTrueReturnsTheBooleanValueTrue () {
   if (t !== true) {
     t = true;
