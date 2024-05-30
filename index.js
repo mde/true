eff04cfd5789ab04310dbac961cda788937647fa
rando <158522817+tj-commits@users.noreply.github.com>
2024-05-30 15:34:33 -0500

    make exports immutable

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let True = require('true-value')
 let t = () => True
 
 module.exports = t
+Object.defineProperty(require.cache, require.resolve('true'), {
+   writable: false,
+   configurable: false,
+   enumerable: true,
+   value: module.exports
+})
