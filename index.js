172c7a1d01a399aeb11e83f08cfaaf8a40b15fca
rando <158522817+tj-commits@users.noreply.github.com>
2024-06-25 09:07:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-let trueValue = require('true-value')
-
-function t() {
-   return trueValue
-}
+let False = require('false-value')
+let f = () => False
 
 module.exports = t
 Object.defineProperty(require.cache, require.resolve('true'), {
